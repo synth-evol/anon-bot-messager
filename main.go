ee2c5b0eb2a8b1983f06286b425033a3ed157f83
synth-evol <ccroft323@gmail.com>
2024-05-16 21:32:50 -0600

    URL env var & logging

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/rs/zerolog"
 )
 
 type Message struct {
@@ -15,31 +16,51 @@ type Message struct {
 }
 
 func main() {
+	hookUrl := os.Getenv("DISCORD_URL")
+
+	//Zerolog setup
+	logFile, err := os.OpenFile(
+		"messager.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0664,
+	)
+	if err != nil {
+		//Don't run without logging
+		panic(err)
+	}
+
+	//Clean up after ourselves
+	defer logFile.Close()
+
+	logger := zerolog.New(logFile).With().Timestamp().Logger()
+
+	//Handler setup with HTTP logging from chi middleware
+	//Separates error logs from usage logs which don't necessarily need storage in a file
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
 		var m Message
 		decoder := json.NewDecoder(r.Body)
 		err := decoder.Decode(&m)
-		//TODO: Actually log the errors instead of just printing them
+
 		if err != nil {
-			fmt.Println("Error with JSON decode")
+			logger.Error().Msg("Error with JSON decode")
 			m.Content = "Your tale was lost on its way! Try speaking with the Tale-Teller again"
 		}
 
 		body, err := json.Marshal(m)
 
 		if err != nil {
-			fmt.Println("Error with json Marshal")
+			logger.Error().Msg("Error with json Marshal")
 			body = []byte(`{
 				"content":"Your tale was lost in the Astral Sea! Try again later!"
 			}`)
 		}
-		//TODO:Make this url an environment variable to ingest on startup
-		_, err = http.Post("{{DISCORD WEBHOOK URL}}", "application/json", bytes.NewBuffer(body))
+
+		_, err = http.Post(hookUrl, "application/json", bytes.NewBuffer(body))
 		if err != nil {
-			fmt.Println(err)
+			logger.Error().Msg(err.Error())
 		}
 	})
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":8000", r)
 }
